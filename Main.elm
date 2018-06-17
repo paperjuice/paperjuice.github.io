06bc547dfc63df416bf17af73543d8d745b6eaf6
paperjuice <paperjuiceing@gmail.com>
2018-06-17 20:35:10 +0200

    Add hotelcriss project

--- Main.elm
+++ Main.elm
@@ -1,125 +1,213 @@
-import Html exposing(Html, div, text, input, button)
-import Html.Attributes exposing(type_, placeholder, class, value)
-import Html.Events exposing(onInput, onClick)
-import Json.Decode as JD
-import Json.Encode as JE
-import WebSocket as WS
+import Html exposing (Html, text, div, img, button, span)
+import Html.Attributes exposing (class, src, style)
+import Navigation exposing (Location)
+import UrlParser exposing (parseHash, s, oneOf, top, map)
 
-
-serverUrl =
-  "ws://172.20.10.2:9998/chat"
-
--- Main --
 main =
-  Html.program
-  { view = chatView
-  , update = chatUpdate
-  , init = chatInit
-  , subscriptions = chatSubscriptions
+  Navigation.program Path
+  { view = view
+  , update = update
+  , init = init
+  , subscriptions = (\_->Sub.none)
   }
 
--- Json Encode --
-messageEncode name msg =
-  JE.object
-  [ ("name", JE.string name)
-  , ("msg",  JE.string msg)
-  ]
 
--- Json Decode --
-type alias JsonMessage =
-  { name : String
-  , msg : String
-  }
 
-messageDecode =
-  JD.map2 JsonMessage
-  ( JD.field "name" JD.string)
-  (JD.field "msg" JD.string)
-
--- Message --
+-- MSG --
 type Msg
-  = SendMessage
-  | AddName
-  | Input String
-  | NewMessage String
+  = Path Location
 
--- Model --
-type alias Message =
-  { name : String
-  , msg : String
-  }
 
+-- ROUTE --
+type Route
+  = Home
+  | Rooms
+  | NotFound
+
+matchRoute : Location -> Route
+matchRoute location =
+  case (parseHash matcher location) of
+    Just route -> route
+    Nothing    -> NotFound
+
+matcher =
+  oneOf
+    [ map Home top
+    , map Rooms (s "rooms")
+    ]
+
+
+-- MODEL --
+type Language = Ro | En
 type alias Model =
-  { name : String
-  , input : String
-  , messages : List Message
+  { route : Route
+  , language: Language
   }
 
--- Init --
-chatInit : (Model, Cmd msg)
-chatInit =
-  ( Model  "" "" [ Message "" "" ], Cmd.none)
+-- INIT --
+init : Location -> (Model, Cmd msg)
+init location =
+  update (Path location) (Model Home Ro)
 
--- View --
-chatView : Model -> Html Msg
-chatView model =
-  case model.name of
-    "" -> initialView model
-    _ -> div[ ]
-            [ div [] (conversationView model)
-            , input [ type_ "text", value model.input, placeholder "Message", onInput Input ] []
-            , button [onClick SendMessage] [text "Send message"]
-            ]
 
-initialView : Model -> Html Msg
-initialView model =
-  div [ class "initialView" ]
-      [ input [ type_ "text", value model.input, placeholder "Enter name", onInput Input ] []
-      , button [ onClick AddName ] [ text "Submit name" ]
+-- UPDATE --
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg model =
+  case msg of
+    Path location ->
+      let
+          newRoute = matchRoute (Debug.log "loc" location)
+      in
+          ( { model | route = newRoute }, Cmd.none )
+
+
+-- VIEW --
+view : Model -> Html msg
+view model =
+  case model.route of
+    Home     -> viewHome model
+    Rooms    -> div [ ] [ text "Rooms kfalsdjfkjsd" ]
+    NotFound -> div [ ] [ text "Nope :(" ]
+
+
+---------------
+-- HOME VIEW --
+---------------
+viewHome : Model -> Html msg
+viewHome model =
+  div [ class "home" ]
+      [ header model
+      , description
+      , container
+      , footer
       ]
 
-conversationView : Model -> List (Html msg)
-conversationView model =
-  List.map (\ msg ->
-    div[] [ text (msg.name ++ "> " ++ msg.msg) ]
-    ) model.messages
+header : Model -> Html msg
+header model =
+  div [ class "header" ]
+      [ div [ class "hotel-criss" ] [ text "Hotel Criss" ]
+      , img [ class "hotel-stars", src "icons/hotel-star.svg" ] [ ]
+      , button [ class "book" ] [ text "Rezerva" ]
+      , div [ class "language" ] (languageColor model.language)
+      ]
+
+description : Html msg
+description =
+  div [ class "description" ]
+      [ div [ class "text" ]
+            [ text "Aici putem pune  o scurta descriere despre Bucuresti, putin despre istoricul hotelului, unde este situtat in capitala, cateva vorbe despre numarul de camere si conditiil excelente pe care le ofere." ]
+      , img [ class "break", src "icons/break.svg" ] [ ]
+      ]
+
+languageColor : Language -> List (Html msg)
+languageColor language =
+  case language of
+    Ro ->
+      languageButtons "#4d4d4d" "white"
+
+    En ->
+      languageButtons "white" "#4d4d4d"
+
+languageButtons : String -> String -> List (Html msg)
+languageButtons colorRo colorEn =
+      [ button [ style [ ("color", colorRo) ] ] [ text "Ro" ]
+      , span [ ] [ text "/" ]
+      , button [ style [ ("color", colorEn) ] ] [ text "En" ]
+      ]
+
+container : Html msg
+container =
+  div [ class "frame" ]
+      [ roomItem
+      , restaurantItem
+      , conferenceItem
+      ]
+
+roomItem : Html msg
+roomItem =
+  div [ class "container" ]
+      [ div [ class "left" ]
+            [ div [ class "picture" ]
+                  [ img [ class "room-block"
+                        , src "https://i.imgur.com/bMYJQje.png"
+                        ] [ ]
+                  ]
+            , descriptionItem "Camere" "Laudam camere nitel, spunem cate sunt in total, si cateva vorbe despre cum e fiecare si ca preturile sunt accesible."
+            ]
+      , div [ class "right" ]
+            [ img [ class "room-right"
+                  , src "https://i.imgur.com/ttSkgoA.jpg"
+                  ] [ ]
+            ]
+      , div [ class "bottom" ]
+            [ img [ class "room-bot"
+                  , src "https://i.imgur.com/69P9RmO.jpg"
+                  ] [ ]
+            ]
+      ]
+
+restaurantItem : Html msg
+restaurantItem =
+  div [ class "container" ]
+      [ div [ class "right" ]
+            [ img [ class "rest-right"
+                  , src "https://i.imgur.com/pFpC8Pp.jpg"
+                  ] [ ]
+            ]
+      , div [ class "left" ]
+            [ div [ class "picture" ]
+                  [ img [ class "rest-left"
+                        , src "https://i.imgur.com/1GGzLwD.jpg"
+                        ] [ ]
+                  ]
+            , descriptionItem "Restaurant" "Spunem despre mancarea pe care o ofera hotelul si contextul in care o poate servi (evenimente)."
+            ]
+      , div [ class "bottom" ]
+            [ img [ class "rest-bot"
+                  , src "https://i.imgur.com/l3YZ8Qe.jpg"
+                  ] [ ]
+            ]
+      ]
+
+conferenceItem : Html msg
+conferenceItem =
+  div [ class "container" ]
+      [ div [ class "left" ]
+            [ div [ class "picture" ]
+                  [ img [ class "conf-left"
+                        , src "https://i.imgur.com/XS6uWXP.jpg"
+                        ] [ ]
+                  ]
+            , descriptionItem "Centru de conferinte" "Ceva despre fatul ca hotelul ofera spatii pentru conferinte."
+            ]
+      , div [ class "right" ]
+            [ img [ class "conf-right"
+                  , src "https://i.imgur.com/D9hXKgc.jpg"
+                  ] [ ]
+            ]
+      , div [ class "bottom" ]
+            [ img [ class "conf-bot"
+                  , src "https://i.imgur.com/8TBYOGM.jpg"
+                  ] [ ]
+            ]
+      ]
+
+descriptionItem : String -> String -> Html msg
+descriptionItem title description=
+  div [ class "text" ]
+      [ div [ class "title" ] [ text title ]
+      , div [ class "description" ]
+            [ text  description ]
+      , detailButton
+      ]
+
+detailButton : Html msg
+detailButton =
+  div [ class "detail-button" ] [ text "Detalii" ]
 
 
--- Update --
-chatUpdate : Msg -> Model -> (Model, Cmd msg)
-chatUpdate msg model =
-  case msg of
-    SendMessage ->
-      let
-          message =
-            messageEncode model.name model.input
-            |> JE.encode 0
-
-          newMessage = Message model.name model.input
-      in
-          ( {model | input = ""}
-          , WS.send serverUrl message
-          )
-
-    Input input ->
-      ( {model | input = input}, Cmd.none )
-
-    AddName ->
-      ( {model | name = model.input, input = ""}, Cmd.none )
-
-    NewMessage message ->
-      let
-          parsedMessage =
-            case JD.decodeString messageDecode message of
-              Ok response -> response
-              error -> JsonMessage "error" "error"
-
-          newMessage = Message parsedMessage.name parsedMessage.msg
-      in
-          ( { model | messages = model.messages ++ [ newMessage ]}, Cmd.none)
-
--- Subscriptions --
-chatSubscriptions : Model -> Sub Msg
-chatSubscriptions model =
-  WS.listen serverUrl NewMessage
-
+footer : Html msg
+footer =
+  div [ class "footer" ]
+      [
+      ]
